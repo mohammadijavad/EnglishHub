e87392ac20a1f639abac65390350cb0405fd5a43
javad <javadMohammadi.js@gmail.com>
2023-11-07 21:47:37 +0330

    why learn with English hub

--- src/pages/WellCome/index.tsx
+++ src/pages/WellCome/index.tsx
@@ -4,10 +4,12 @@ import Button from '../../components/cubes/Button';
 import CoverSection from '../../assets/images/welcome/first cover section .jpg';
 import justKeep from '../../assets/images/welcome/justKeep.png';
 import chat from '../../assets/images/welcome/Chat.png';
+import sample from '../../assets/images/welcome/first cover section .jpg';
 function Welcome() {
   return (
     <Landing>
-      <div className="h-[90vh] w-full  container mx-auto flex items-center justify-evenly md:justify-between relative flex-col md:flex-row">
+      {/* first section */}
+      <div className="h-screen w-full  container mx-auto flex items-center justify-evenly md:justify-between relative flex-col md:flex-row">
         <div className="w-full md:w-1/2 md:h-full flex items-center justify-center flex-col">
           <h1 className="text-xl md:text-3xl text-center leading-10">
             <p className="text-BrightPurple mb-2 bold">انگلیس هاب </p>
@@ -24,7 +26,7 @@ function Welcome() {
         </div>
         <div className="w-3/4 md:w-1/2  md:h-full flex items-center justify-center ">
           <img
-            className=" w-full md:w-3/4"
+            className=" w-5/6 md:w-3/4"
             src={CoverSection}
             alt="English learning Girl"
           />
@@ -42,6 +44,88 @@ function Welcome() {
             alt="just keep going"
             className="w-3/12 opacity-50 transform -rotate-45"
           />
+        </div>
+      </div>
+      {/* second section */}
+      <div className="h-screen w-full container mx-auto mt-4 md:mt-0">
+        <div className="flex justify-center ">
+          <p className="p-3 rounded px-6 bold text-black bg-gray-400">
+            چرا انگلیس هاب
+          </p>
+        </div>
+        <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-rows-2 gap-4 mt-8 ">
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>پادکست های متنوع</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>تعیین سطح رایگان و استاندارد</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>تماشای ویدیو و آزمون</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>مسابقه با هم سطح ها</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>مترجم و بخش یادگیری لغات</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p> جعبه لاینتر</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>محاسبه سرعت رشد وداشبورد اختصاصی</p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
+          <div className=" p-2 flex flex-col justify-center items-center">
+            <p>هوش مصنوعی </p>
+            <p className="text-sm mt-2">
+              کلی پادکست های کوتاه برای سطح های مختلف
+            </p>
+            <div className="w-full flex justify-center mt-2">
+              <img src={sample} alt="listen podcast" className="w-1/2" />
+            </div>
+          </div>
         </div>
       </div>
     </Landing>
