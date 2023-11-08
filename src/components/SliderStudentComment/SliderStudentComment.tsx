4253688ac2829a9d67c5cf45ba12ff6a48726b58
javad <javadMohammadi.js@gmail.com>
2023-11-08 23:57:42 +0330

    add swiper card carousel to comments

--- src/components/SliderStudentComment/SliderStudentComment.tsx
+++ src/components/SliderStudentComment/SliderStudentComment.tsx
@@ -1,6 +1,26 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React, { useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCards } from 'swiper/modules';
 import { comments } from '../constants/Comments';
 export default function SimpleSlider() {
-  return <></>;
+  return (
+    <>
+      <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
